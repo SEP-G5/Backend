433b9e5343f1298c525c08ea4b3a9c0e8f0ac0fa
Filip Björklund <Afisse13@gmail.com>
2019-11-19 11:43:06 +0100

    Added random nonce and fixed timestamp
    * Added a random nonce that is generated randomly when the block is created.
    * Fixed the timestamp to only store the seconds (like bitcoin)

--- src/blockchain/block.rs
+++ src/blockchain/block.rs
@@ -1,4 +1,5 @@
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use rand::Rng;
+use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
 use std::convert::TryInto;
 use std::time::{SystemTime, UNIX_EPOCH};
@@ -9,14 +10,19 @@ use std::time::{SystemTime, UNIX_EPOCH};
 ///
 type HashType = [u8; 32];
 
+/// Value for an empty (0) hash value.
+///
+const EMPTY_HASH: HashType = [0; 32];
+
 // ========================================================================== //
 
 /// Represents a block in the blockchain
 ///
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Block<T: AsRef<[u8]>> {
     /// Hash of the parent block
-    parent: u64,
-    /// Timestamp for when the block was generated. Specified in UNIX epoch
+    parent: HashType,
+    /// Timestamp for when the block was generated in seconds since UNIC epoch.
     timestamp: u64,
     /// Random nonce
     rand_nonce: u64,
@@ -31,13 +37,14 @@ pub struct Block<T: AsRef<[u8]>> {
 impl<T: AsRef<[u8]>> Block<T> {
     /// Create a new block with the specified parent and data
     ///
-    pub fn new(parent: u64, data: T) -> Block<T> {
-        let rand_nonce = 0;
+    pub fn new(parent: HashType, data: T) -> Block<T> {
+        let mut rng = rand::thread_rng();
+        let rand_nonce: u64 = rng.gen();
         let nonce = 0;
         let timestamp = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .expect("Could not retrieve a correct UNIX epoch timestamp")
-            .as_millis() as u64;
+            .as_secs() as u64;
         Block {
             parent,
             timestamp,
@@ -45,6 +52,14 @@ impl<T: AsRef<[u8]>> Block<T> {
             nonce,
             data,
         }
+    }
+
+    /// Create a block from a parent block. The hash for the parent block is
+    /// calculated before the children is created.
+    ///
+    pub fn from_parent(parent: &Block<T>, data: T) -> Block<T> {
+        let parent_hash = parent.calc_hash();
+        Block::new(parent_hash, data)
     }
 
     /// Returns the timestamp when the block was created. In UNIX epoch
@@ -59,7 +74,7 @@ impl<T: AsRef<[u8]>> Block<T> {
         self.nonce
     }
 
-    /// Set the nonce of the block to the specified value.
+    /// Set the noHashTypek to the specified value.
     ///
     pub fn set_nonce(&mut self, nonce: u64) {
         self.nonce = nonce;
@@ -79,7 +94,7 @@ impl<T: AsRef<[u8]>> Block<T> {
     ///
     pub fn calc_hash(&self) -> HashType {
         let mut hasher = Sha256::new();
-        hasher.input(self.parent.to_le_bytes());
+        hasher.input(self.parent);
         hasher.input(self.timestamp.to_le_bytes());
         hasher.input(self.rand_nonce.to_le_bytes());
         hasher.input(self.nonce.to_le_bytes());
@@ -94,9 +109,13 @@ impl<T: AsRef<[u8]>> Block<T> {
 
 // ========================================================================== //
 
-impl<T: AsRef<[u8]>> Serialize for Block<T> {
-    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        serializer.serialize_u64(self.parent)
+impl<T: AsRef<[u8]> + PartialEq> PartialEq for Block<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.parent == other.parent
+            && self.timestamp == other.timestamp
+            && self.rand_nonce == other.rand_nonce
+            && self.nonce == other.nonce
+            && self.data == other.data
     }
 }
 
@@ -116,12 +135,13 @@ pub fn hash_to_str(hash: &HashType) -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde_json;
 
     /// Test for the creation of a new block
     ///
     #[test]
     fn test_block_create() {
-        let block = Block::<&str>::new(0, "Hello");
+        let block = Block::<&str>::new(EMPTY_HASH, "Hello");
         assert_eq!(
             block.get_nonce(),
             0,
@@ -134,7 +154,7 @@ mod tests {
     ///
     #[test]
     fn test_block_hash() {
-        let block = Block::<&str>::new(0, "World");
+        let block = Block::<&str>::new(EMPTY_HASH, "World");
 
         assert_eq!(
             block.calc_hash(),
@@ -147,8 +167,8 @@ mod tests {
     ///
     #[test]
     fn test_block_timestamp() {
-        let block_0 = Block::<&str>::new(0, "a string");
-        let block_1 = Block::<&str>::new(0, "a string");
+        let block_0 = Block::<&str>::new(EMPTY_HASH, "a string");
+        let block_1 = Block::<&str>::new(EMPTY_HASH, "a string");
         assert!(block_0.get_timestamp() <= block_1.get_timestamp());
     }
 
@@ -156,8 +176,9 @@ mod tests {
     ///
     #[test]
     fn test_block_serde() {
-        let block = Block::<&str>::new(0, "some str");
-
-        //let ser = serde_
+        let block = Block::<&str>::new(EMPTY_HASH, "some str");
+        let ser = serde_json::to_string(&block).unwrap();
+        let de: Block<&str> = serde_json::from_str(&ser).unwrap();
+        assert_eq!(block, de);
     }
 }
