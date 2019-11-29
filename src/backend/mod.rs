be536d9664f3ed7254ab6df678a69ff091bad90c
Christoffer Gustafsson <christoffer.golf@gmail.com>
2019-11-29 12:38:09 +0100

    Will now broadcast a block to the p2p network when it is mined.

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -129,8 +129,7 @@ impl Backend {
                                 .expect("Failed to send");
                         } else {
                             self.enqueue_tx(transaction.clone());
-                            let packet = Packet::PostTx(transaction);
-                            network.broadcast(packet);
+                            network.broadcast(Packet::PostTx(transaction));
                             res.send(Ok(())).expect("Failed to send");
                         }
                     }
@@ -138,14 +137,14 @@ impl Backend {
             }
 
             // Step the mining process once
-            self.mine_step();
+            self.mine_step(&network);
 
             //std::thread::sleep(std::time::Duration::from_millis(5));
         }
     }
 
     /// Run one step of the mining process
-    fn mine_step(&mut self) {
+    fn mine_step(&mut self, network: &Network) {
         // Set currently mined block
         if self.txs.len() > 0 && self.mined.is_none() {
             let tx = self.txs.pop_front().unwrap();
@@ -166,7 +165,8 @@ impl Backend {
             // Add block to blockchain and broadcast it
             let block = self.mined.take().expect("Mined cannot be 'None' here");
             println!("Successfully mined a block");
-            self.chain.push(block, false).expect("Failed to push block");
+            self.chain.push(block.clone(), false).expect("Failed to push block");
+            network.broadcast(Packet::PostBlock(block));
         }
     }
 
